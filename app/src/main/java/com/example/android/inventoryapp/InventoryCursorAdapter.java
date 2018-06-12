ab02e2e786574949db9e3ecd38c9186ba60a3f5a
Tan Nguyen <ban5hien@yahoo.com>
2018-06-12 14:36:08 -0700

    add draft for cursor adapter

---
+++ app/src/main/java/com/example/android/inventoryapp/InventoryCursorAdapter.java
@@ -0,0 +1,26 @@
+package com.example.android.inventoryapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+public class InventoryCursorAdapter extends CursorAdapter {
+
+
+    public InventoryCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+    }
+}
