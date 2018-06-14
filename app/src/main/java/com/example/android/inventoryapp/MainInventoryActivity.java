a5c3b7d2451a86eb4ea91b68fea1be9d8aa42961
Tan Nguyen <ban5hien@yahoo.com>
2018-06-14 16:01:41 -0700

    update the MainActivity to run and display cursor on list items

--- app/src/main/java/com/example/android/inventoryapp/MainInventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainInventoryActivity.java
@@ -16,6 +16,23 @@ import android.widget.TextView;
 
 import com.example.android.inventoryapp.data.InventoryContract.InventoryEntry;
 import com.example.android.inventoryapp.data.InventoryDbHelper;
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainInventoryActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
@@ -39,75 +56,44 @@ public class MainInventoryActivity extends AppCompatActivity implements
             }
         });
 
+        // Set empty view
         ListView inventoryListView = findViewById(R.id.list);
         View emptyView = findViewById(R.id.empty_view);
         inventoryListView.setEmptyView(emptyView);
 
+        // Set cursorAdapter to inventory list
         mCursorAdapter = new InventoryCursorAdapter(this, null);
         inventoryListView.setAdapter(mCursorAdapter);
 
+        // Setup the item click listener
+        inventoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(MainInventoryActivity.this, EditorActivity.class);
+                Uri currentPetUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
+                intent.setData(currentPetUri);
+                startActivity(intent);
+            }
+        });
+
+        // Start loader
         getLoaderManager().initLoader(INVENTORY_LOADER, null, this);
     }
 
-    private void insertData() {
-        ContentValues values = new ContentValues();
-        values.put(InventoryEntry.COLUMN_PRODUCT_NAME, "Laptop");
-        values.put(InventoryEntry.COLUMN_PRICE, 500);
-        values.put(InventoryEntry.COLUMN_QUANTITY, 1);
-
-        Uri newUri = getContentResolver().insert(InventoryEntry.CONTENT_URI, values);
-    }
-
-    private Cursor queryData() {
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         String[] projection = {
                 InventoryEntry._ID,
                 InventoryEntry.COLUMN_PRODUCT_NAME,
                 InventoryEntry.COLUMN_PRICE,
-                InventoryEntry.COLUMN_QUANTITY,
-                InventoryEntry.COLUMN_SUPPLIER_NAME,
-                InventoryEntry.COLUMN_SUPPLIER_PHONE_NUMBER};
+                InventoryEntry.COLUMN_QUANTITY};
 
-        Cursor cursor = db.query(
-                InventoryEntry.TABLE_NAME,
+        return new CursorLoader(this,
+                InventoryEntry.CONTENT_URI,
                 projection,
-                null, null,
-                null, null,
+                null,
+                null,
                 null);
-
-        TextView displayView = findViewById(R.id.list);
-        displayView.append(InventoryEntry._ID + " - " +
-                InventoryEntry.COLUMN_PRODUCT_NAME + " - " +
-                InventoryEntry.COLUMN_PRICE + " - " +
-                InventoryEntry.COLUMN_QUANTITY + "\n");
-
-
-        int idColumnIndex = cursor.getColumnIndex(InventoryEntry._ID);
-        int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_NAME);
-        int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_PRICE);
-        int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_QUANTITY);
-
-        // Iterate through all the returned rows in the cursor
-        while (cursor.moveToNext()) {
-            int currentID = cursor.getInt(idColumnIndex);
-            String currentName = cursor.getString(nameColumnIndex);
-            int currentPrice = cursor.getInt(priceColumnIndex);
-            int currentQuantity = cursor.getInt(quantityColumnIndex);
-
-            // Display the values from each column of the current row in the cursor in the TextView
-            displayView.append(("\n" + currentID + " - " +
-                    currentName + " - " +
-                    currentPrice + " - " +
-                    currentQuantity));
-        }
-        cursor.close();
-        return cursor;
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
     }
 
     @Override
