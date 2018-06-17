5adf4d5c17e7eca0a8c4868793d698fedf9f6915
hero5jungle <ban5hien@yahoo.com>
2018-06-17 12:44:31 -0700

    clean projectt

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryProvider.java
@@ -1,12 +1,6 @@
 package com.example.android.inventoryapp.data;
 
 import android.content.ContentProvider;
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
